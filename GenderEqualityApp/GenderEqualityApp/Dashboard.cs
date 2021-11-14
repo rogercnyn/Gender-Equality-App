69b03338164b4ae8c3d06c5dced6d06b762a7112
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2021-11-14 18:18:14 +0800

    Add Info Tab Content

--- GenderEqualityApp/GenderEqualityApp/Dashboard.cs
+++ GenderEqualityApp/GenderEqualityApp/Dashboard.cs
@@ -61,5 +61,10 @@ namespace GenderEqualityApp
         {
             this.Close();
         }
+
+        private void bunifuLabel72_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
