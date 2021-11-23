5f83a600ce02d04ab3ae086db1e9ae44e85d7ef2
Roger Canayon Jr <rcanayonjr@gmail.com>
2021-11-23 14:28:49 +0800

    Add Picture

--- GenderEqualityApp/GenderEqualityApp/RegForm.cs
+++ GenderEqualityApp/GenderEqualityApp/RegForm.cs
@@ -8,13 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.IO;
 
 namespace GenderEqualityApp
 {
     public partial class RegForm : Form
     {
-        /*SqlConnection conn = new SqlConnection("Server = localhost\\SQLEXPRESS; Database=usersdatabase;Trusted_Connection=True;");
-        SqlCommand cmd;*/
+        SqlConnection conn = new SqlConnection("Server = localhost\\SQLEXPRESS; Database=usersdatabase;Trusted_Connection=True;");
+        SqlCommand cmd;
+        public string imgLoc;
         public RegForm()
         {
             InitializeComponent();
@@ -22,11 +24,15 @@ namespace GenderEqualityApp
 
         private void bunifuButton21_Click(object sender, EventArgs e)
         {
-            /*bool allFill = tbxUN.Text != string.Empty && tbxPW.Text != string.Empty && tbxFN.Text != string.Empty && tbxMN.Text != string.Empty && tbxLN.Text != string.Empty && tbxG.Text != string.Empty && tbxEA.Text != string.Empty && tbxBday.Text != string.Empty;
+            bool allFill = tbxUN.Text != string.Empty && tbxPW.Text != string.Empty && tbxFN.Text != string.Empty && tbxMN.Text != string.Empty && tbxLN.Text != string.Empty && tbxG.Text != string.Empty && tbxEA.Text != string.Empty && tbxBday.Text != string.Empty;
             if  (allFill)
             {
                 conn.Open();
-                cmd = new SqlCommand("INSERT INTO usersdata VALUES ('" + tbxUN.Text + "','" + tbxPW.Text + "','" + tbxFN.Text + "','" + tbxMN.Text + "','" + tbxLN.Text + "','" + tbxG.SelectedItem.ToString() + "','" + tbxEA.Text + "','" + tbxBday.Text + "')", conn);
+                byte[] img; 
+                FileStream fs = new FileStream(imgLoc, FileMode.Open, FileAccess.Read); 
+                BinaryReader br = new BinaryReader(fs); 
+                img = br.ReadBytes((int)fs.Length);
+                cmd = new SqlCommand("INSERT INTO usersdata VALUES ('" + tbxUN.Text + "','" + tbxPW.Text + "','" + tbxFN.Text + "','" + tbxMN.Text + "','" + tbxLN.Text + "','" + tbxG.SelectedItem.ToString() + "','" + tbxEA.Text + "','" + tbxBday.Text + "','" + img + "')", conn);
                 cmd.ExecuteNonQuery();
                 MessageBox.Show("Registered Successfully!");
                 tbxUN.Text = string.Empty;
@@ -37,6 +43,7 @@ namespace GenderEqualityApp
                 tbxG.Text = string.Empty;
                 tbxEA.Text = string.Empty;
                 tbxBday.Text = string.Empty;
+                regPic.ImageLocation = null;
                 conn.Close();
                 this.Hide();
                 LogIn loginform = new LogIn();
@@ -47,8 +54,19 @@ namespace GenderEqualityApp
             if (!allFill)
             {
                 MessageBox.Show("Fill up all of the text box.");
-            }*/
+            }
 
+        }
+
+        private void bunifuButton22_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Title = "Choose a file";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                imgLoc = dlg.FileName.ToString();
+                regPic.ImageLocation = imgLoc;
+            }
         }
 
         private void btnMin_Click(object sender, EventArgs e)
@@ -59,6 +77,31 @@ namespace GenderEqualityApp
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void tbxFN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxMN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
